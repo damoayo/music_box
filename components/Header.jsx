034f933ab46eecc06a64ff8d71e6557e5ff20d4b
MJ JI <mamapai@naver.com>
2025-01-08 20:21:28 +0900

    header scroll

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,14 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import UserIcon from "@/components/UserLcon";
+import React, { useEffect, useRef, useState } from "react";
+import UserIcon from "@/components/UserIcon";
 import PagePadding from "@/components/PagePadding";
 import { FaChromecast } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
-import Logo from "@/components/elements/Logo"; // Import the Logo component
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import Logo from "./elements/Logo";
 import Navigator from "./elements/Navigator";
+import { cn } from "@/lib/utils";
 
 const HeaderDrawer = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,10 +17,8 @@ const HeaderDrawer = ({ children }) => {
     <Drawer direction="left" open={isOpen} onOpenChange={setIsOpen}>
       <DrawerTrigger>{children}</DrawerTrigger>
       <DrawerContent className="w-[240px] h-full">
-        {/* 로고 */}
-        {/* 네비게이션+재생목록 */}
         <div className="py-3">
-          <div className="py-3">
+          <div className="px-3">
             <Logo
               isInDrawer
               onClickClose={() => {
@@ -43,14 +33,27 @@ const HeaderDrawer = ({ children }) => {
   );
 };
 
-const Header = ({ children }: { children: React.ReactNode }) => {
-  const [] = useState(false);
+const Header = ({ children }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const headRef = useRef();
+
   useEffect(() => {
-    
+    const handleScroll = () => {
+      const scrollValue = headRef?.current?.scrollTop;
+      // console.log(">scrollValue", scrollValue);
+      setIsScrolled(scrollValue !== 0);
+    };
+
+    headRef?.current?.addEventListener("scroll", handleScroll);
+    return () => {
+      headRef?.current?.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
   return (
-    <header className="relative overflow-y-auto w-full h-full">
-      <section className="absolute top-0 w-full">
+    <header ref={headRef} className="relative overflow-y-auto w-full h-full">
+      {/* bgSection */}
+      <section className=" absolute top-0 w-full">
         <div className="relative h-[400px] w-full">
           <Image
             className="object-cover"
@@ -58,16 +61,21 @@ const Header = ({ children }: { children: React.ReactNode }) => {
             src="https://images.unsplash.com/photo-1680552396773-2a942321e750?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="빌딩들 속의 빨간 이층버스"
           />
-          <div className="absolute h-[400px] top-0 bg-black opacity-30 w-full "></div>
-          <div className="absolute h-[400px] top-0 bg-gradient-to-t from-black  w-full "></div>
+          <div className=" absolute h-[400px] top-0 bg-black opacity-40 w-full"></div>
+          <div className=" absolute h-[400px] top-0 bg-gradient-to-t from-black w-full"></div>
         </div>
       </section>
       {/* searchSection */}
-
-      <section className="sticky top-0 left-0 z-10">
+      <section
+        className={cn("sticky top-0 left-0 z-10", isScrolled && "bg-black")}
+      >
         <PagePadding>
           <div className="h-[64px] flex flex-row justify-between items-center">
-            <article className="h-[42px] min-w-[480px] hidden lg:flex flex-row items-center gap-[16px] bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px]">
+            <article
+              className="h-[42px] min-w-[480px] hidden lg:flex flex-row items-center
+            bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px] gap-[16px] border border-neutral-500
+            "
+            >
               <div>
                 <FiSearch size={24} />
               </div>
@@ -82,9 +90,8 @@ const Header = ({ children }: { children: React.ReactNode }) => {
                 <Logo />
               </article>
             </HeaderDrawer>
-
             <article className="flex flex-row gap-6 items-center">
-              <FaChromecast size={24} />
+              <FaChromecast size={26} />
               <UserIcon />
             </article>
           </div>
